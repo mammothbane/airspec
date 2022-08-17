e5433837e19a99201572b18283010aa7ad3b740f
Patrick Chwalek <patrickchwalek@gmail.com>
2022-08-17 11:16:12 -0400

    thermopile code compiles

--- STM32_WPAN/App/hrs_app.h
+++ STM32_WPAN/App/hrs_app.h
@@ -1,52 +1,39 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    App/app_ble.h
-  * @author  MCD Application Team
-  * @brief   Header for ble application
+  * File Name          : App/hrs_app.h
+  * Description        : Header for hrs_application.c module
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef APP_BLE_H
-#define APP_BLE_H
+#ifndef __HRS_APP_H
+#define __HRS_APP_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "hci_tl.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "cmsis_os2.h"
+
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
-
-    typedef enum
-    {
-      APP_BLE_IDLE,
-      APP_BLE_FAST_ADV,
-      APP_BLE_LP_ADV,
-      APP_BLE_SCAN,
-      APP_BLE_LP_CONNECTING,
-      APP_BLE_CONNECTED_SERVER,
-      APP_BLE_CONNECTED_CLIENT
-    } APP_BLE_ConnStatus_t;
-
 /* USER CODE BEGIN ET */
 
 /* USER CODE END ET */
@@ -58,27 +45,22 @@ extern "C" {
 
 /* External variables --------------------------------------------------------*/
 /* USER CODE BEGIN EV */
-extern osThreadId_t LinkConfigProcessId;
+
 /* USER CODE END EV */
 
-/* Exported macro ------------------------------------------------------------*/
+/* Exported macros ------------------------------------------------------------*/
 /* USER CODE BEGIN EM */
 
 /* USER CODE END EM */
 
 /* Exported functions ---------------------------------------------*/
-  void APP_BLE_Init( void );
-
-  APP_BLE_ConnStatus_t APP_BLE_Get_Server_Connection_Status(void);
-
+void HRSAPP_Init( void );
 /* USER CODE BEGIN EF */
-  void LinkConfiguration(void *argument);
-  uint8_t APP_BLE_ComputeCRC8(uint8_t *DataPtr, uint8_t Datalen);
-  void BLE_SVC_L2CAP_Conn_Update_7_5(void);
+
 /* USER CODE END EF */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /*APP_BLE_H */
+#endif /*__HRS_APP_H */
