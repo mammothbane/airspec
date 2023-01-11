a4365134c652efd677a349bea7e0246854aa6758
Nathan Perry <np@nathanperry.dev>
2023-01-11 04:26:15 -0500

    py, nix: init dependency management

---
+++ flake.nix
@@ -0,0 +1,36 @@
+# vim: ft=nix :
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  description = "airspec/captivates";
+
+  outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      py3 = pkgs.python3.withPackages (pypkgs: with pypkgs; [
+        poetry
+      ]);
+
+    in {
+      devShells.default = pkgs.mkShell {
+        pname = "airspec devenv";
+        version = self.rev or "dirty";
+
+        venvDir = "python/.venv";
+
+        buildInputs = with pkgs; [
+          python3.pkgs.venvShellHook
+          py3
+        ];
+      };
+    })
+  );
+}
