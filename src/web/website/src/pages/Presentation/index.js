8a3f1f2a6239413f96a6568b1e0089083b2255e6
Nathan Perry <np@nathanperry.dev>
2023-02-01 10:15:28 -0500

    website: delete commented code from Presentation

--- src/web/website/src/pages/Presentation/index.js
+++ src/web/website/src/pages/Presentation/index.js
@@ -62,8 +62,6 @@ function Presentation() {
         sticky
       />
 
-
-
       <MKBox
         minHeight="75vh"
         width="100%"
@@ -75,16 +73,14 @@ function Presentation() {
           placeItems: "center",
         }}
       >
-
-
         <Container>
 
-        <div
-          className="moving-clouds">
-        <div className="fog-low">
-                 <img alt="..." src={cloudImage} />
-               </div>
-               </div>
+          <div
+            className="moving-clouds">
+            <div className="fog-low">
+              <img alt="..." src={cloudImage} />
+            </div>
+          </div>
 
           <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
 
@@ -109,17 +105,10 @@ function Presentation() {
               px={{ xs: 6, lg: 12 }}
               mt={1}
             >
-
               Free & Open Source Smart Eyeglass Platform Designed to Measure You and the Surrounding Environment
-
-              {/* Web UI Kit built over ReactJS &amp; MUI. Join over 1.6 million
-              developers around the world. */}
             </MKTypography>
-
           </Grid>
-
         </Container>
-
       </MKBox>
 
       <Card
@@ -133,60 +122,9 @@ function Presentation() {
           boxShadow: ({ boxShadows: { xxl } }) => xxl,
         }}
       >
-
         <Counters />
         <DesignBlocks />
-        {/* <Information />
-        <Pages /> */}
-        {/* <Container sx={{ mt: 6 }}>
-          <BuiltByDevelopers />
-        </Container> */}
-        {/* <Container>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                variant="gradient"
-                color="info"
-                icon="flag"
-                title="Getting Started"
-                description="Check the possible ways of working with our product and the necessary files for building your own project."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/overview/material-kit/",
-                  label: "Let's start",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                color="info"
-                icon="precision_manufacturing"
-                title="Plugins"
-                description="Get inspiration and have an overview about the plugins that we used to create the Material Kit."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/overview/datepicker/",
-                  label: "Read more",
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <FilledInfoCard
-                color="info"
-                icon="apps"
-                title="Components"
-                description="Material Kit is giving you a lot of pre-made components, that will help you to build UI's faster."
-                action={{
-                  type: "external",
-                  route: "https://www.creative-tim.com/learning-lab/react/alerts/material-kit/",
-                  label: "Read more",
-                }}
-              />
-            </Grid>
-          </Grid>
-        </Container> */}
-        {/* <Testimonials /> */}
-        {/* <Download /> */}
+
         <MKBox pt={18} pb={6}>
           <Container>
             <Grid container spacing={3}>
@@ -194,75 +132,38 @@ function Presentation() {
                 <MKTypography variant="h4" fontWeight="bold" mb={0.5}>
                   Questions? Send us an {" "}
                   <MKTypography
-        component="a"
-        href="mailto: chwalek@mit.edu"
-        target="_blank"
-        rel="noreferrer"
-        variant="h4"
-        fontWeight="bold"
-        color="info"
-      >
-        email!
-      </MKTypography>
+                    component="a"
+                    href="mailto: chwalek@mit.edu"
+                    target="_blank"
+                    rel="noreferrer"
+                    variant="h4"
+                    fontWeight="bold"
+                    color="info"
+                  >
+                    email!
+                  </MKTypography>
                 </MKTypography>
                 <MKTypography variant="body1" color="text">
                   Interested in seeing our last smart eyeglass iteration?{" "}
                   <MKTypography
-        component="a"
-        href="https://captivate.media.mit.edu/"
-        target="_blank"
-        rel="noreferrer"
-        variant="body1"
-        fontWeight="bold"
-        color="info"
-      >
-        Project Captivate
-      </MKTypography>
+                    component="a"
+                    href="https://captivate.media.mit.edu/"
+                    target="_blank"
+                    rel="noreferrer"
+                    variant="body1"
+                    fontWeight="bold"
+                    color="info"
+                  >
+                    Project Captivate
+                  </MKTypography>
                 </MKTypography>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                lg={5}
-                my={{ xs: 5, lg: "auto" }}
-                mr={{ xs: 0, lg: "auto" }}
-                sx={{ textAlign: { xs: "center", lg: "right" } }}
-              >
-                {/* <MKSocialButton
-                  component="a"
-                  href="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23mui5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-kit-react"
-                  target="_blank"
-                  color="twitter"
-                  sx={{ mr: 1 }}
-                >
-                  <i className="fab fa-twitter" />
-                  &nbsp;Tweet
-                </MKSocialButton>
-                <MKSocialButton
-                  component="a"
-                  href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-kit-react"
-                  target="_blank"
-                  color="facebook"
-                  sx={{ mr: 1 }}
-                >
-                  <i className="fab fa-facebook" />
-                  &nbsp;Share
-                </MKSocialButton>
-                <MKSocialButton
-                  component="a"
-                  href="https://www.pinterest.com/pin/create/button/?url=https://www.creative-tim.com/product/material-kit-react"
-                  target="_blank"
-                  color="pinterest"
-                >
-                  <i className="fab fa-pinterest" />
-                  &nbsp;Pin it
-                </MKSocialButton> */}
               </Grid>
             </Grid>
           </Container>
         </MKBox>
       </Card>
-      <MKBox pt={6} px={1} mt={6}>
+
+      <MKBox px={1}>
         <DefaultFooter content={footerRoutes} />
       </MKBox>
     </>
