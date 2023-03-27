7a824247a001ef338fd90e17030fc524c5dca57f
Nathan Perry <np@nathanperry.dev>
2023-03-27 07:42:02 -0400

    ingest: attempt to improve csv dump performance

--- src/svc/ingest/src/endpoints/dump.rs
+++ src/svc/ingest/src/endpoints/dump.rs
@@ -4,16 +4,21 @@ use std::{
 };
 
 use async_compat::CompatExt;
-use async_std::stream::StreamExt as _;
 use futures::TryStreamExt;
 use influxdb2::{
     models::Query,
     Client,
 };
+use smol::{
+    stream,
+    stream::{
+        Stream,
+        StreamExt,
+    },
+};
 use tide::{
     Body,
     Response,
-    Status,
     StatusCode,
 };
 use tracing::Instrument;
@@ -26,11 +31,11 @@ pub struct State {
     pub influx:     Arc<Client>,
 }
 
-#[derive(serde::Deserialize)]
-struct DumpRequest {
+#[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Deserialize)]
+pub struct DumpRequest {
     id:    String,
-    start: String,
-    end:   String,
+    start: i64,
+    end:   i64,
 }
 
 lazy_static::lazy_static! {
@@ -48,44 +53,135 @@ fn test_inject(s: &str, name: &str) -> Result<(), tide::Error> {
     ))
 }
 
-pub async fn dump(req: tide::Request<impl Deref<Target = State>>) -> tide::Result {
-    let DumpRequest {
-        id,
+fn base_query(
+    state: &State,
+    &DumpRequest {
         start,
         end,
-    } = req.query::<DumpRequest>()?;
-
-    test_inject(&id, "id")?;
-    test_inject(&start, "start")?;
-    test_inject(&end, "end")?;
-
-    let state = req.state();
-
-    let query = format!(
+        ref id,
+    }: &DumpRequest,
+) -> String {
+    format!(
         r#"
         from(bucket: "{}")
             |> range(start: {start}, stop: {end})
             |> drop(columns: ["_start", "_stop"])
             |> filter(fn: (r) => r.system_uid == "{id}")
-            |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
         "#,
-        state.influx_cfg.bucket,
-    );
+        &state.influx_cfg.bucket,
+    )
+}
 
-    tracing::debug!(%id, %start, %end, %query, "requesting csv");
-
-    let req = state
+#[tracing::instrument(skip(state))]
+pub async fn get_count(state: &State, req: &DumpRequest) -> tide::Result<usize> {
+    let ret = state
         .influx
-        .query_raw_stream(&state.influx_cfg.org, Some(Query::new(query)))
+        .query_raw(
+            &state.influx_cfg.org,
+            Some(Query::new(format!(
+                r#"
+        {} |> count()
+        "#,
+                base_query(state, req)
+            ))),
+        )
+        .compat()
+        .await?;
+
+    let ret = ret.trim();
+    if ret.is_empty() {
+        tracing::debug!("no rows");
+        return Ok(0);
+    }
+
+    tracing::debug!(?ret);
+
+    let x = serde_json::from_str::<serde_json::Value>(ret)?;
+    tracing::debug!(count = ?x);
+
+    Ok(0)
+}
+
+fn make_dump_query(state: &State, req: &DumpRequest, limit: usize, offset: usize) -> String {
+    format!(
+        r#"
+            {}
+            |> limit(n: {limit}, offset: {offset})
+        "#,
+        base_query(state, req),
+    )
+}
+
+#[tracing::instrument(skip(client))]
+pub async fn dump_once(
+    client: Arc<Client>,
+    org: String,
+    query: String,
+) -> Box<dyn Stream<Item = Result<bytes::Bytes, futures::io::Error>> + Send + Sync + Unpin + 'static>
+{
+    tracing::debug!("requesting csv");
+
+    match client
+        .query_raw_stream(&org, Some(Query::new(query)))
         .compat()
         .instrument(tracing::debug_span!("influx dump query"))
         .await
-        .status(StatusCode::BadGateway)?
-        .map(|x| x.map_err(|e| futures::io::Error::new(futures::io::ErrorKind::BrokenPipe, e)))
-        .into_async_read();
+    {
+        Ok(s) => {
+            let err_mapped = s.map(|x| {
+                x.map_err(|e| futures::io::Error::new(futures::io::ErrorKind::BrokenPipe, e))
+            });
 
-    let bufread = async_std::io::BufReader::new(req);
-    let body = Body::from_reader(bufread, None);
+            Box::new(err_mapped)
+        },
+        Err(e) => Box::new(stream::once(Err(futures::io::Error::new(
+            futures::io::ErrorKind::BrokenPipe,
+            e,
+        )))),
+    }
+}
+
+pub async fn dump(
+    req: tide::Request<impl Deref<Target = State> + Send + Sync + Unpin + 'static>,
+) -> tide::Result {
+    let dump_req = req.query::<DumpRequest>()?;
+
+    tracing::debug!(?dump_req, "dump request");
+
+    test_inject(&dump_req.id, "id")?;
+
+    const LIMIT: usize = 10000;
+
+    let state = req.state();
+    let count = get_count(state, &dump_req).await?;
+
+    tracing::info!(?count);
+
+    let influx = state.influx.clone();
+    let org = state.influx_cfg.org.to_string();
+
+    let queries = (0..count)
+        .step_by(LIMIT)
+        .map(|n| make_dump_query(state, &dump_req, LIMIT, n))
+        .collect::<Vec<_>>();
+
+    let reqs = stream::iter(queries)
+        .then(move |query| {
+            let influx = influx.clone();
+            let org = org.clone();
+
+            async move {
+                let ret = dump_once(influx, org, query).await;
+
+                tracing::info!("single dump request");
+
+                ret
+            }
+        })
+        .flat_map(|x| x);
+
+    let reader = reqs.into_async_read();
+    let body = Body::from_reader(Box::pin(reader), None);
 
     Ok(Response::builder(StatusCode::Ok).content_type("text/csv").body(body).build())
 }
