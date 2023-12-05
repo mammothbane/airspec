e923b3a2b9509c20b76e90e8617c8500735030ef
Nathan Perry <np@nathanperry.dev>
2023-12-05 09:51:45 -0500

    nixos/airspecs: fix grub device

--- nix/nixos/airspecs/hardware.nix
+++ nix/nixos/airspecs/hardware.nix
@@ -1,4 +1,4 @@
-{
+{ lib, ... }: {
   swapDevices = [
     {
       device = "/swap";
@@ -7,6 +7,8 @@
   ];
 
   nix.settings.max-jobs = 1;
+
+  boot.loader.grub.device = lib.mkForce "/dev/nvme0n1";
 
   # filesystems are handled by aws_support.nix, image.nix
 }
