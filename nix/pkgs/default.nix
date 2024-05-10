eded9f97929bdd8c0b6efe8ceb9f0bc76223e4d9
Nathan Perry <np@nathanperry.dev>
2024-05-10 14:35:58 -0400

    nix/pkgs: drop swift_protobuf

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,7 +1,6 @@
 { pkgs, self ? null }: {
   docs-site       = pkgs.callPackage ../../src/web/docs     { inherit self; };
   website         = pkgs.callPackage ../../src/web/website  { inherit self; };
-  swift_protobuf  = pkgs.callPackage ./swift_protobuf.nix   {};
   ingest          = pkgs.callPackage ../../src/svc/ingest   {};
   grpc_swift      = pkgs.callPackage ./grpc_swift.nix       {};
   # firmware        = pkgs.callPackage ./firmware             { inherit self; };
